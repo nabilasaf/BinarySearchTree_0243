15e3f92a63f9738ddf471708bfd68a23986ced0e
nabilasaf <nabilasafitri861@gmail.com>
2024-06-13 15:28:46 +0700

    Mark the new node as ROOT

--- Binarysearch3.cpp
+++ Binarysearch3.cpp
@@ -40,6 +40,9 @@ public:
         search(element, parent, currentNode); //Locate the Node which will be the parent of the node to be insert
 
         if (parent == nullptr) //If the parent is NULL (Tree is empty)
+        {
+            ROOT = newNode; //Mark the new node as ROOT
+        }
     }
     
 };
