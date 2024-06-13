31e24cc7b7d48eb79bab1e6c56aaeda1f0e85969
nabilasaf <nabilasafitri861@gmail.com>
2024-06-13 16:18:36 +0700

    melengkapi case 5

--- Binarysearch3.cpp
+++ Binarysearch3.cpp
@@ -160,6 +160,11 @@ int main()
            case '5':
             {
               return 0;
+              default:
+              {
+                cout << "Invalid option" << endl;
+                break;
+              }
             }
         }
     }
